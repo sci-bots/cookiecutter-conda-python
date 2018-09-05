b6c98547a897d6d5535d8c2431e1931ca66be622
Christian Fobel <christian@fobel.net>
2018-09-05 11:39:11 -0400

    feat(py3): add support for Python 3

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_module_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_module_name}}/__init__.py
@@ -1,3 +1,6 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
